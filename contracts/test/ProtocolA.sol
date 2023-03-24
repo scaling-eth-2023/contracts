3b5e0aafa2b52b08c3310ae54282c9c384c74f83
Kari <evergreenkary@gmail.com>
2023-03-24 18:07:34 +0800

    Implement mock protocol

--- contracts/test/ProtocolA.sol
+++ contracts/test/ProtocolA.sol
@@ -1,8 +1,41 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-contract ProtocolA {
-    function execute() external returns (bool) {
-        return true;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ProtocolA is ERC20 {
+    IERC20 tokenA;
+
+    constructor(address tokenAAddress) ERC20("TOKENB", "TKB") {
+        tokenA = IERC20(tokenAAddress);
+    }
+
+    // User swaps their token A with the exact same amount of token B.
+    function swapExactTokenAWithTokenB(uint256 _amount) external {
+        uint256 userTokenABalance = tokenA.balanceOf(msg.sender);
+        require(
+            userTokenABalance >= _amount,
+            "USER DOESN'T HAVE ENOUGH TOKEN A BALANCE"
+        );
+
+        uint256 allowance = tokenA.allowance(msg.sender, address(this));
+        require(allowance >= _amount, "NOT ENOUGH ALLOWANCE");
+
+        tokenA.transferFrom(msg.sender, address(this), _amount);
+        _mint(msg.sender, _amount);
+    }
+
+    function swapExactTokenBWithTokenA(uint256 _amount) external {
+        uint256 userTokenBBalance = balanceOf(msg.sender);
+        require(
+            userTokenBBalance >= _amount,
+            "USER DOESN'T HAVE ENOUGH TOKEN B BALANCE"
+        );
+
+        uint256 allowance = allowance(msg.sender, address(this));
+        require(allowance >= _amount, "NOT ENOUGH ALLOWANCE");
+
+        tokenA.transfer(msg.sender, _amount);
+        _burn(msg.sender, _amount);
     }
 }
